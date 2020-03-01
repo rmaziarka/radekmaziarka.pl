5ac63f26ecdc219e738f63a6823564ce3671edac
Radek Maziarka <maziarka.radoslaw@outlook.com>
2020-03-01 21:59:34 +0100

    Copy images to the static folders

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var inputFile =  process.argv[3];
 var outputDir = process.argv[4];
 
 var mergeComments = (process.argv[5] == 'm')?'m':'s' ;
-
+var groupedImages = [];
 
 
 if (fs.existsSync(outputDir)) {
@@ -69,6 +69,34 @@ else {
     return;
 }
 
+var myMkdirSync = function(dir){
+    if (fs.existsSync(dir)){
+        return
+    }
+
+    try{
+        fs.mkdirSync(dir)
+    }catch(err){
+        if(err.code == 'ENOENT'){
+            myMkdirSync(path.dirname(dir)) //create parent dir
+            myMkdirSync(dir) //create dir
+        }
+    }
+}
+
+function groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach((item) => {
+         const key = keyGetter(item);
+         const collection = map.get(key);
+         if (!collection) {
+             map.set(key, [item]);
+         } else {
+             collection.push(item);
+         }
+    });
+    return map;
+}
 
 
 
@@ -76,165 +104,190 @@ else {
 function wordpressImport(backupXmlFile, outputDir){
     var parser = new xml2js.Parser();
 
-    fs.readFile(backupXmlFile, function(err, data) {
-        parser.parseString(data, function (err, result) {
-            if (err) {
-                console.log(`Error parsing xml file (${backupXmlFile})\n${JSON.stringify(err)}`); 
-                return 1;
-            }
-            // console.dir(result); 
-            // console.log(JSON.stringify(result)); return;
-            var posts = [];
+
+    fs.readFile('media.xml', function(err, data) {
+        parser.parseString(data, (err, result) => {
+            handleImagesXML(result);
             
-            // try {
-                posts = result.rss.channel[0].item;
-                
-                console.log(`Total Post count: ${posts.length}`);
-
-                posts = posts.filter(function(post){
-                    var status = '';
-                    if(post["wp:status"]){
-                        status = post["wp:status"].join(''); 
-                    }
-                    // console.log(post["wp:status"].join(''));
-                    return status != "private" && status != "inherit" 
-                });
-
-
-                // console.log(posts)
-                console.log(`Post count: ${posts.length}`);
-
-                var title = '';
-                var slug = '';
-                var content = '';
-                var tags = [];
-                var categoryName = '';
-                var published = '';
-                var comments = [];
-                var fname = '';
-                var markdown = '';
-                var fileContent = '';
-                var fileHeader = '';
-                var postMaps = {};
-                
-                posts.forEach(function(post){
-                    var postMap = {};
-
-                    title = post.title[0].trim();
-                    slug = post.link[0].trim().replace('https://radekmaziarka.pl','');
-                    
-                    // console.log(title);
-
-                    // if (title && title.indexOf("'")!=-1){
-                    title = title.replace(/'/g, "''");
-                    // }
-
-                    published = post.pubDate;
-                    comments = post['wp:comment'];
-                    fname = post["wp:post_name"][0] || post["wp:post_id"];
-                    markdown = '';
-                    // if (post.guid && post.guid[0] && post.guid[0]['_']){
-                    //     fname = path.basename(post.guid[0]['_']);
-                    // }
-                    // console.log(comments);
-
-                    console.log(`\n\n\n\ntitle: '${title}'`);
-                    console.log(`published: '${published}'`);
-                    
-                    if (comments){
-                        console.log(`comments: '${comments.length}'`);    
-                    }
-                    
-                    tags = [];
-
-                    var categories = post.category;
-                    var tagString = '';
-
-                    if (categories && categories.length){
-                        categories.forEach(function (category){
-
-                            if(category.$.domain === 'post_tag')
-                                tags.push(category['_']);
-                            else if (category.$.domain === 'category')
-                                categoryName = category['_'];
-                        });
-
-                        // console.log(tags.join(", "));
-                        // tags = tags.join(", ");
-                        tagString = 'tags: [\'' + tags.join("', '") + "']\n";
-                        // console.log(tagString);
-                    }
-
-                    var pmap = {fname:'', comments:[]};
-                    pmap.fname = outputDir+'/'+fname+'-comments.md';
-
-                    fname = outputDir+'/'+fname+'.md';
-                    pmap.postName = fname;
-                    console.log(`fname: '${fname}'`);
-                    
-                    if (post["content:encoded"]){
-                        // console.log('content available');
-                        content = '<div>'+post["content:encoded"]+'</div>'; //to resolve error if plain text returned
-                        markdown = tds.turndown(content);
-                        // console.log(markdown);
-
-                        fileHeader = `---\ntitle: '${title}'\nslug: '${slug}'\ndate: ${published}\ndraft: false\ncategory: '${categoryName}'\n${tagString}---\n`;
-                        fileContent = `${fileHeader}\n${markdown}`;
-                        pmap.header = `${fileHeader}\n`;
-
-                        // fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n\n${markdown}`;
-
-                        writeToFile(fname, fileContent);
-                        
-                    }
-
-                    //comments:
-                    /*
-                        "wp:comment" [.each]
-                            wp:comment_author[0]
-                            wp:comment_author_email[0]
-                            wp:comment_author_url[0]
-                            wp:comment_date[0]
-                            wp:comment_content[0]
-                            wp:comment_approved[0] == 1
-                        wp:post_id
-
-                    */
-                    var comments = post["wp:comment"] || [];
-                    // console.dir(comments);
-                    var anyApprovedComments = 0;
-                    var ccontent = '';
-                    comments.forEach(function(comment){
-                        // console.log('')
-                        if(comment["wp:comment_approved"].pop()){
-                            anyApprovedComments = 1;
-
-                            var cmt = {title:'', published:'', content:'', author:{}};
-
-                            cmt.published = (comment["wp:comment_date"]?comment["wp:comment_date"].pop():'');
-
-                            var cont = '<div>'+comment["wp:comment_content"].pop()+'</div>';
-                            cmt.content = (comment["wp:comment_content"]?tds.turndown(cont):'');
-
-                            cmt.author.name = (comment["wp:comment_author"]?comment["wp:comment_author"].pop():'');
-                            cmt.author.email = (comment["wp:comment_author_email"]?comment["wp:comment_author_email"].pop():'');
-                            cmt.author.url = (comment["wp:comment_author_url"]?comment["wp:comment_author_url"].pop():'');
-
-                            ccontent += `#### [${cmt.author.name}](${cmt.author.url} "${cmt.author.email}") - ${cmt.published}\n\n${cmt.content}\n<hr />\n`;
-
-                            pmap.comments.push(cmt);
-                        }
-                    });
-
-                    //just a hack to re-use blogger writecomments method
-                    if (pmap && pmap.comments && pmap.comments.length){
-                        writeComments({"0": pmap});
-                    }
-
-                });
-
+            // fs.readFile(backupXmlFile, function(err, data) {
+            //     parser.parseString(data, handlePostsXML);
+            // });
         });
     });
+}
+
+function handleImagesXML(result){
+    var images = result.rss.channel[0].item;
+
+    groupedImages = groupBy(images, image => image['wp:post_parent']);
+
+    images.forEach((image) =>{
+        var url = image['guid'][0]['_'];
+        var imagePath = url.replace(/https:\/\/radekmaziarka.pl\/wp-content\/|https:\/\/radblog.pl\/wp-content\/|http:\/\/radblog.pl\/wp-content\//,'');
+
+        var folderPath = 'quickstart/static/images/' + imagePath.replace('uploads/','').substring(0, 7);
+        myMkdirSync(folderPath);
+
+        var targetFilePath = 'quickstart/static/images/' + imagePath.replace('uploads/','');
+        fs.createReadStream(imagePath).pipe(fs.createWriteStream(targetFilePath));
+    })
+}
+
+function handlePostsXML(err, result) {
+    if (err) {
+        console.log(`Error parsing xml file (${backupXmlFile})\n${JSON.stringify(err)}`); 
+        return 1;
+    }
+    // console.dir(result); 
+    // console.log(JSON.stringify(result)); return;
+    var posts = [];
+    
+    // try {
+        posts = result.rss.channel[0].item;
+        
+        console.log(`Total Post count: ${posts.length}`);
+
+        posts = posts.filter(function(post){
+            var status = '';
+            if(post["wp:status"]){
+                status = post["wp:status"].join(''); 
+            }
+            // console.log(post["wp:status"].join(''));
+            return status != "private" && status != "inherit" 
+        });
+
+
+        // console.log(posts)
+        console.log(`Post count: ${posts.length}`);
+
+        var title = '';
+        var slug = '';
+        var content = '';
+        var tags = [];
+        var categoryName = '';
+        var published = '';
+        var comments = [];
+        var fname = '';
+        var markdown = '';
+        var fileContent = '';
+        var fileHeader = '';
+        var postMaps = {};
+        
+        posts.forEach(function(post){
+            var postMap = {};
+
+            title = post.title[0].trim();
+            slug = post.link[0].trim().replace('https://radekmaziarka.pl','');
+            
+            // console.log(title);
+
+            // if (title && title.indexOf("'")!=-1){
+            title = title.replace(/'/g, "''");
+            // }
+
+            published = post.pubDate;
+            comments = post['wp:comment'];
+            fname = post["wp:post_name"][0] || post["wp:post_id"];
+            markdown = '';
+            // if (post.guid && post.guid[0] && post.guid[0]['_']){
+            //     fname = path.basename(post.guid[0]['_']);
+            // }
+            // console.log(comments);
+
+            console.log(`\n\n\n\ntitle: '${title}'`);
+            console.log(`published: '${published}'`);
+            
+            if (comments){
+                console.log(`comments: '${comments.length}'`);    
+            }
+            
+            tags = [];
+
+            var categories = post.category;
+            var tagString = '';
+
+            if (categories && categories.length){
+                categories.forEach(function (category){
+
+                    if(category.$.domain === 'post_tag')
+                        tags.push(category['_']);
+                    else if (category.$.domain === 'category')
+                        categoryName = category['_'];
+                });
+
+                // console.log(tags.join(", "));
+                // tags = tags.join(", ");
+                tagString = 'tags: [\'' + tags.join("', '") + "']\n";
+                // console.log(tagString);
+            }
+
+            var pmap = {fname:'', comments:[]};
+            pmap.fname = outputDir+'/'+fname+'-comments.md';
+
+            fname = outputDir+'/'+fname+'.md';
+            pmap.postName = fname;
+            console.log(`fname: '${fname}'`);
+            
+            if (post["content:encoded"]){
+                // console.log('content available');
+                content = '<div>'+post["content:encoded"]+'</div>'; //to resolve error if plain text returned
+                markdown = tds.turndown(content);
+                // console.log(markdown);
+
+                fileHeader = `---\ntitle: '${title}'\nslug: '${slug}'\ndate: ${published}\ndraft: false\ncategory: '${categoryName}'\n${tagString}---\n`;
+                fileContent = `${fileHeader}\n${markdown}`;
+                pmap.header = `${fileHeader}\n`;
+
+                // fileContent = `---\ntitle: '${title}'\ndate: ${published}\ndraft: false\n${tagString}---\n\n${markdown}`;
+
+                writeToFile(fname, fileContent);
+                
+            }
+
+            //comments:
+            /*
+                "wp:comment" [.each]
+                    wp:comment_author[0]
+                    wp:comment_author_email[0]
+                    wp:comment_author_url[0]
+                    wp:comment_date[0]
+                    wp:comment_content[0]
+                    wp:comment_approved[0] == 1
+                wp:post_id
+
+            */
+            var comments = post["wp:comment"] || [];
+            // console.dir(comments);
+            var anyApprovedComments = 0;
+            var ccontent = '';
+            comments.forEach(function(comment){
+                // console.log('')
+                if(comment["wp:comment_approved"].pop()){
+                    anyApprovedComments = 1;
+
+                    var cmt = {title:'', published:'', content:'', author:{}};
+
+                    cmt.published = (comment["wp:comment_date"]?comment["wp:comment_date"].pop():'');
+
+                    var cont = '<div>'+comment["wp:comment_content"].pop()+'</div>';
+                    cmt.content = (comment["wp:comment_content"]?tds.turndown(cont):'');
+
+                    cmt.author.name = (comment["wp:comment_author"]?comment["wp:comment_author"].pop():'');
+                    cmt.author.email = (comment["wp:comment_author_email"]?comment["wp:comment_author_email"].pop():'');
+                    cmt.author.url = (comment["wp:comment_author_url"]?comment["wp:comment_author_url"].pop():'');
+
+                    ccontent += `#### [${cmt.author.name}](${cmt.author.url} "${cmt.author.email}") - ${cmt.published}\n\n${cmt.content}\n<hr />\n`;
+
+                    pmap.comments.push(cmt);
+                }
+            });
+
+            //just a hack to re-use blogger writecomments method
+            if (pmap && pmap.comments && pmap.comments.length){
+                writeComments({"0": pmap});
+            }
+
+        });
 
 }
 

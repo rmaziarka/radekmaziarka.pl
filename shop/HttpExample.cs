bac093f45d4fb92a81c67e3c7b61f380ed674734
rmaziarka <maziarka.radoslaw@outlook.com>
2022-12-07 20:02:07 +0100

    Add shop basic Azure Functions

---
+++ shop/HttpExample.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Stripe;
+using Stripe.Checkout;
+
+namespace shop
+{
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            StripeConfiguration.ApiKey = 
+                System.Environment.GetEnvironmentVariable("StripeKey", EnvironmentVariableTarget.Process);;
+            var domain = "https://radekmaziarka.pl";
+            var options = new SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+                        Price = "price_1M30MiFecJIMSlsEqP1AYu2y",
+                        Quantity = 1,
+                        TaxRates = new List<string>(){"txr_1M30NFFecJIMSlsEcarDkVSz"}
+                    },
+                },
+                TaxIdCollection = new SessionTaxIdCollectionOptions(){ Enabled = true},
+                Mode = "payment",
+                SuccessUrl = domain + "/success.html",
+                CancelUrl = domain + "/cancel.html",
+            };
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            return new RedirectResult(session.Url, false);
+        }
+    }
+}
---
+++ shop/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace shop;
+
+public class Program
+{
+    public static void Main()
+    {
+        var host = new HostBuilder()
+            .ConfigureFunctionsWorkerDefaults()
+            .Build();
+
+        host.Run();
+    }
+}
